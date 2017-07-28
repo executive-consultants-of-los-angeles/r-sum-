0ec52e6746e6dc576a9904a7e357be65f6829027
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-28 01:49:54 +0000

    Let's print less. See #11.

--- files/rsum/rsum/home/models.py
+++ files/rsum/rsum/home/models.py
@@ -16,17 +16,12 @@ class CV(models.Model):
     def check_sections(self):
         cv_i = CV.objects.all()
         if not cv_i.exists():
-            print('load it up!')
             cv_f = open('/srv/rsum/cv.yml')
             cv_d = yaml.load(cv_f.read())        
-            print(json.dumps(cv_d.get('cv'), indent=2))
             self.save_sections(cv_d.get('cv'))
 
     def save_sections(self, cv_d):
-        print(json.dumps(cv_d, indent=2))
         for k, v in cv_d.iteritems():
-            print(k)
-            print(json.dumps(v, indent=2))
             current = CV()
             current.section_name = k 
             current.save()
@@ -41,7 +36,8 @@ class Section(models.Model):
     name = models.CharField(max_length=200)
     
     
-    def save_sub_sections(self, cv, sub_sections):
+    def save_sub_sections(self, section, sub_sections):
+        print(section)
         print(sub_sections)
     
 class SubSection(models.Model):
