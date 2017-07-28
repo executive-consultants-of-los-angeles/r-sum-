f0d41327e8207a6e276e207c0dafad5e0418f111
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-28 21:25:56 +0000

    Updated to return section headings. See #13.

--- files/rsum/rsum/home/models.py
+++ files/rsum/rsum/home/models.py
@@ -25,10 +25,9 @@ class CV(models.Model):
             return cv_i
 
     def get_cv(self, cv_id):
-        for i in range(1,len(models.Section.objects.all())+1):
-            return Section.objects.filter(
-                cv = cv_id 
-            )
+        return list(Section.objects.filter(
+            cv = cv_id 
+        ).values())
 
     def save_cv(self, cv):
         cv_i = CV()
