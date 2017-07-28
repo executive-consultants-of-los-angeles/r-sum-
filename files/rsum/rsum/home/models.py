680f655d03d435828d1b612ebf0238f226ba75b6
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-28 01:28:56 +0000

    Dump cv_d See #11.

--- files/rsum/rsum/home/models.py
+++ files/rsum/rsum/home/models.py
@@ -19,6 +19,7 @@ class CV(models.Model):
             print('load it up!')
             cv_f = open('/srv/rsum/cv.yml')
             cv_d = yaml.load(cv_f.read())        
+            print(json.dumps(cv_d, indent=2))
             self.save_sections(cv_d.get('cv'))
 
     def save_sections(self, cv_d):
@@ -26,6 +27,7 @@ class CV(models.Model):
             current = CV()
             current.section_name = item 
             current.save()
+        return None
 
 
 class Section(models.Model):
