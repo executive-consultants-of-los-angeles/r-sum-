33e51dadcb427b9cdc5da7fed2f8a4f22186b36f
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-28 19:54:32 +0000

    Next one. See #13.

--- files/rsum/rsum/home/models.py
+++ files/rsum/rsum/home/models.py
@@ -69,7 +69,7 @@ class SubSection(models.Model):
                 ss_i.value_type = type(sub_section)
                 ss_i.name = k
                 ss_i.save()
-                p = Projects() 
+                p = Project() 
                 p.save_project_dict(v, ss_i)
         elif type(sub_section) == type(list()):
             for c,i in enumerate(sub_section):
