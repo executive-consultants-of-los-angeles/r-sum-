e81248f914521ac96838f7bcddae140f5245c19f
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-28 01:03:01 +0000

    Import CV, check sections. See #11.

--- files/rsum/rsum/home/views.py
+++ files/rsum/rsum/home/views.py
@@ -1,9 +1,16 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from home.models import CV
+
 # Create your views here.
 
 def index(request):
+
+    cv = CV()
+    cv.check_sections()
+
     return render(request, 'home/index.html', {})
