c027dac1a0f266f865243f1f9eeb8d89fb075461
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-28 22:34:37 +0000

    Much cleaner. See #13.

--- files/rsum/rsum/home/views.py
+++ files/rsum/rsum/home/views.py
@@ -11,9 +11,6 @@ import models
 # Create your views here.
 
 def index(request):
-
-    sections = []
-    
     cv_i = models.CV(id=1)
     cv_i.check_sections()
 
@@ -21,35 +18,8 @@ def index(request):
     
     print(json.dumps(cv, indent=1)) 
 
-    for i in range(0,len(sections)):
-        ss = models.SubSection()
-        subsections = list(ss.get_sub_section(sections[i].get('id')))
-        if len(subsections) > 0:
-            sections[i].update({'subsections': subsections})
-            for index, value in enumerate(subsections):
-                p = models.Project()
-                projects = list(p.get_projects(value.get('id')))[0]
-                if projects.get('value') != u"<type 'dict'>":
-                    sections[i].get('subsections')[index].update({'value':projects})
-                else:
-                    pi = models.ProjectItems()
-                    p_items = list(pi.get_project_items(projects.get('id')))
-                    sections[i].get('subsections')[index].update({'value':p_items})
-                    for p_index, p_value in enumerate(p_items):
-                        if p_value.get('value') != u"<type 'dict'>":
-                            sections[i].get('subsections')[index].get('value')[p_index].update({'value': p_value}) 
-                        else:
-                            pe = models.ProjectEntry()
-                            p_entries = list(pe.get_entry(p_value.get('id'))) 
-                            sections[i].get('subsections')[index].get('value')[p_index].update({'value': p_entries})
-                            for pe_index, pe_value in enumerate(p_entries):
-                                el = models.EntryListItem()
-                                el_items = list(el.get_list_item(pe_value.get('id')))                              
-                                sections[i].get('subsections')[index].get('value')[p_index].get('value')[pe_index].update({'value': el_items})
-
-
     context = {
-        'sections': sections 
+        'cv': cv 
     }
 
     return render(request, 'home/index.html', {})
