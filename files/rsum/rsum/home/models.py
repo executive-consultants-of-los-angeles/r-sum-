ca83e46021c921536d8cf1fcb37a95899f89a935
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-28 00:34:04 +0000

    Updated models, adjusted indent on cv yaml. See #11.

--- files/rsum/rsum/home/models.py
+++ files/rsum/rsum/home/models.py
@@ -7,3 +7,25 @@ from django.db import models
 # Create your models here.
 class CV(models.Model):
     section = models.CharField(max_length=200)
+
+
+class Section(models.Model):
+    cv = models.ForeignKey(CV, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    
+class SubSection(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    
+
+class Projects(models.Model):
+    sub_section = models.ForeignKey(SubSection, on_delete=models.CASCADE)
+
+
+class ProjectItemsList(models.Model):
+    projects = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+
+class ProjectItem(models.Model):
+    project_item_list = models.ForeignKey(ProjectItemsList, on_delete=models.CASCADE)
+    value = models.CharField(max_length=200) 
