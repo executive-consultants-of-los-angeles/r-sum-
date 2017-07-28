0e4a3d7b213b459c545164eefb716c3ea735faa6
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-28 01:04:47 +0000

    Print cv instance. See #11.

--- files/rsum/rsum/home/views.py
+++ files/rsum/rsum/home/views.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from __future__ import print_function
 
 from django.shortcuts import render
 
-from home.models import CV
+from .models import CV
 
 # Create your views here.
 
 def index(request):
 
     cv = CV()
+    print(cv.objects.all())
     cv.check_sections()
 
     return render(request, 'home/index.html', {})
