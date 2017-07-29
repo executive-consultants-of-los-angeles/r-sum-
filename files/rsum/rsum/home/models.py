69bd87881e3f5b80558108cf642fa943a71b9f86
A. H. Laughlin <mal@gahan-corporation.com>
2017-07-29 21:13:42 +0000

    Added order field. See #17.

--- files/rsum/rsum/home/models.py
+++ files/rsum/rsum/home/models.py
@@ -52,6 +52,7 @@ class Section(models.Model):
     cv = models.ForeignKey(CV, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, default='section')
     value = models.CharField(max_length=200, null=True) 
+    order = models.IntegerField(default=0)
     iterable = models.BooleanField(default=False)
 
     def get_sections(self, cv):
