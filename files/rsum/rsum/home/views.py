4b1593a1cb21ebc3b502323a61c764bfaf8d2d80
A. H. Laughlin <mal@gahan-corporation.com>
2017-08-03 06:29:08 +0000

    Add views. See #34.

--- files/rsum/rsum/home/views.py
+++ files/rsum/rsum/home/views.py
@@ -6,23 +6,25 @@ from __future__ import print_function
 from django.shortcuts import render
 from django.db import models
 
-from schema.cv import CV 
+from schema.cv import CV
 from schema.projectitem import ProjectItem
 
 import json
 
-# Create your views here.
 
 def index(request):
     cv_i = CV(
-        id = 1
+        id=1
     )
 
     cv_i.check_sections()
     cv = cv_i.get_cv()
 
     sections = []
-    for section in sorted(cv.get('sections').items(), key = lambda t: t[1].get('id')):
+    for section in sorted(
+        cv.get('sections').items(),
+        key=lambda t: t[1].get('id')
+    ):
         sections.append({section[0]: section[1]})
 
     context = {
@@ -30,8 +32,8 @@ def index(request):
     }
 
     skills = context.get('cv')[2].get('skills').get('content')
-    skillset = {} 
-    for index,skill in enumerate(skills):
+    skillset = {}
+    for index, skill in enumerate(skills):
         for content in skill.get('content'):
             if content.get('name') == 'name':
                 skillset.update({
@@ -40,7 +42,7 @@ def index(request):
                     }
                 })
 
-    for name,value in skillset.iteritems():
+    for name, value in skillset.iteritems():
         for content in skills[value.get('index')].get('content'):
             if content.get('name') == 'competence':
                 skillset.get(name).update({
@@ -49,25 +51,42 @@ def index(request):
             elif content.get('name') != 'id' and content.get('name') != 'name':
                 skillset.get(name).update({
                     content.get('name'): {
-                        'experience': content.get('content').get(content.get('name')).get('experience'),
-                        'name': content.get('content').get(content.get('name')).get('name'),
-                        'competence': content.get('content').get(content.get('name')).get('competence'),
+                        'experience': content.get(
+                            'content'
+                        ).get(
+                            content.get('name')
+                        ).get('experience'),
+                        'name': content.get(
+                            'content'
+                        ).get(
+                            content.get('name')
+                        ).get('name'),
+                        'competence': content.get(
+                            'content'
+                        ).get(
+                            content.get('name')
+                        ).get('competence'),
                     }
                 })
     print(json.dumps(skillset, indent=2))
     context.get('cv')[2].update({
         'skills': skillset,
-    })            
+    })
 
     values = {}
-    values_list = context.get('cv')[3].get('values').get('content')[1].get('content')
+    values_list = context.get(
+        'cv'
+    )[3].get('values').get('content')[1].get('content')
     for i in values_list:
         name = i.get('content').items()[0][0]
         content = i.get('content').items()[0][1]
-        values.update({name:content}) 
+        values.update({name: content})
 
     values_list = []
-    for value in sorted(values.items(), key = lambda t: t[1].get('id')):
+    for value in sorted(
+        values.items(),
+        key=lambda t: t[1].get('id')
+    ):
         values_list.append(value)
     values = values_list
     context.get('cv')[3].get('values').get('content')[1].update({
@@ -76,25 +95,39 @@ def index(request):
 
     experience = {}
     experience_list = context.get('cv')[4].get('experience').get('content')[1:]
-    for k,v in enumerate(experience_list):
-        #print(json.dumps(v,indent=1))
-        for p, i in v.get('content')[5].get('content').get('projects').iteritems():
+    for k, v in enumerate(experience_list):
+        # print(json.dumps(v,indent=1))
+        for p, i in v.get(
+            'content'
+        )[5].get('content').get('projects').iteritems():
             j = i.strip('[').strip(']').split("', '")
-            experience_list[k].get('content')[5].get('content').get('projects').update({p:[]})
+            experience_list[k].get(
+                'content'
+            )[5].get('content').get('projects').update({p: []})
             for l in j:
                 l = l.replace("'", '')
-                experience_list[k].get('content')[5].get('content').get('projects').get(p).append(l)
-        #print(json.dumps(v.get('content')[5].get('content').get('projects'), indent=1))
+                experience_list[k].get(
+                    'content'
+                )[5].get('content').get('projects').get(p).append(l)
+        # print(json.dumps(v.get(
+        #    'content'
+        # )[5].get('content').get('projects'), indent=1))
     context.get('cv')[4].get('experience').update({
         'experience': experience_list
     })
-    #print(json.dumps(context.get('cv')[3].get('values').get('content')[1], indent=1))
-    #print(type(values))
+    # print(json.dumps(context.get(
+    #    'cv'
+    # )[3].get('values').get('content')[1], indent=1))
+    # print(type(values))
 
-    education = context.get('cv')[5].get('education').get('content')[3].get('content')[0]
-    projects = education.get('content').replace("'",'').replace("[",'').replace("]",'').split(", ")
+    education = context.get(
+        'cv'
+    )[5].get('education').get('content')[3].get('content')[0]
+    projects = education.get(
+        'content'
+    ).replace("'", '').replace("[", '').replace("]", '').split(", ")
     context.get('cv')[5].get('education').update({
         'projects': projects
     })
-    
+
     return render(request, 'home/index.html', context)
