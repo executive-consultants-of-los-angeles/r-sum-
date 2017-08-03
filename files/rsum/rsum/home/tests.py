dc8bfddb583f940a9498e2b0c86590084d11d5dc
A. H. Laughlin <mal@gahan-corporation.com>
2017-08-03 05:10:53 +0000

    First shot. See #33.

--- files/rsum/rsum/home/tests.py
+++ files/rsum/rsum/home/tests.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from home.schema.entryitem import EntryItem
+from home.schema.entry import Entry
 
-# Create your tests here.
+import home
+
+home.Entry = Entry()
+
+class EntryItemTestCase(TestCase):
+    def setUp(self):
+        EntryItem.objects.create(content="item")
+    
+    def test_entry_item(self):
+        item = EntryItem.objects.get(content="item")
+        self.assertEqual(item.content,'item')
