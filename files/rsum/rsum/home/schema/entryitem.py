372f1d76ee46a041b778426b031e91e74abac0c5
A. H. Laughlin <mal@gahan-corporation.com>
2017-08-03 06:00:18 +0000

    Might just be able to to nix this guy all together. See #34.

--- files/rsum/rsum/home/schema/entryitem.py
+++ files/rsum/rsum/home/schema/entryitem.py
@@ -5,31 +5,31 @@ from __future__ import print_function
 
 from django.db import models
 
+
 class EntryItem(models.Model):
     entry = models.ForeignKey('home.Entry', on_delete=models.CASCADE)
     value = models.CharField(max_length=200, null=True)
-    
+
     def get_list_item(self, entry):
         items = []
         for item in list(
             EntryItem.objects.filter(
-                entry = entry
+                entry=entry
             ).values()
         ):
             items.append(item)
         return items
-            
 
     def save_list_item(self, list_item, pe):
-        if type(list_item) == type(list()):
+        if isinstance(list_itemi, list):
             for i in list_item:
                 eli = EntryItem()
                 eli.entry = pe
                 eli.value = i
                 eli.save()
-            return eli 
+            return eli
 
-        if type(list_item) == type(str()):
+        if isinstance(list_item, str):
             eli = EntryItem()
             eli.entry = pe
             eli.value = list_item
