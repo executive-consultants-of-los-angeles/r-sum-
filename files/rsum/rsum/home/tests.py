804e8eedfbfd12edafb4ed0de7bd256987452465
A. H. Laughlin <mal@gahan-corporation.com>
2017-08-03 05:13:21 +0000

    Add remaining models. See #33.

--- files/rsum/rsum/home/tests.py
+++ files/rsum/rsum/home/tests.py
@@ -3,11 +3,22 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from home.schema.cv import CV
+from home.schema.section import Section
+from home.schema.subsection import SubSection
+from home.schema.project import Project
+from home.schema.projectitem import ProjectItem
 from home.schema.entryitem import EntryItem
 from home.schema.entry import Entry
 
 import home
 
+home.CV = CV()
+home.Section = Section()
+home.SubSection = SubSection()
+home.Project = Project()
+home.ProjectItem = ProjectItem()
+home.EntryItem = EntryItem()
 home.Entry = Entry()
 
 class EntryItemTestCase(TestCase):
