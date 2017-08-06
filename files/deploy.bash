3970872e18578e486c11952a7b06e9ac7489f594
A. H. Laughlin <alex@ecla.solutions>
2017-08-06 16:40:43 -0700

    Update deploy. See #53.

--- files/deploy.bash
+++ files/deploy.bash
@@ -2,12 +2,12 @@
 docker rm -f apsql
 docker rm -f arsum
 docker rm -f angos
-docker build -t apsql:latest alex/apsql
+docker build -t apsql:latest /src/rsum/files/alex/apsql
 docker run -d --name apsql -p 5432:5432 -h apsql apsql /usr/bin/supervisord -n
-ansible-playbook alex/apsql/p.yml
-docker build -t arsum:latest alex/arsum
+ansible-playbook /src/rsum/files/alex/apsql/p.yml
+docker build -t arsum:latest /src/rsum/files/alex/arsum
 docker run -d --name arsum -p 8192:8192 -h arsum --link apsql arsum /usr/bin/supervisord -n
-ansible-playbook alex/arsum/p.yml
-docker build -t angos:latest alex/angos
+ansible-playbook /src/rsum/files/alex/arsum/p.yml
+docker build -t angos:latest /src/rsum/files/alex/angos
 docker run -d --name angos -p 8192:8192 -h angos --link apsql --link arsum angos /usr/bin/supervisord -n
-ansible-playbook alex/angos/p.yml
+ansible-playbook /src/rsum/files/alex/angos/p.yml
