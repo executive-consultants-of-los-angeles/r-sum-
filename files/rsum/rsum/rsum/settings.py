ed848be4eef331ba79cc026957b7ce6d2500d1fa
A. H. Laughlin <alex@ecla.solutions>
2017-08-06 16:48:22 -0700

    Fix db host. See #53.

--- files/rsum/rsum/rsum/settings.py
+++ files/rsum/rsum/rsum/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'rsum.wsgi.application'
 if socket.gethostname() == 'mrsum': 
     DB_HOST = 'mpsql'
     DB_PORT = '5433'
-else:
-    DB_HOST = 'pg'
+elif socket.gethostname() == 'arsum':
+    DB_HOST = 'apsql'
     DB_PORT = '5432'
 
 DATABASES = {
