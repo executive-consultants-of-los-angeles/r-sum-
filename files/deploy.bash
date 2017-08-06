dd8321651c1880bfc2aa5b62021551397dacaad1
A. H. Laughlin <alex@ecla.solutions>
2017-08-06 13:29:31 -0700

    Detach. See #53.

--- files/deploy.bash
+++ files/deploy.bash
@@ -3,11 +3,11 @@ docker rm -f apsql
 docker rm -f arsum
 docker rm -f angos
 docker build -t apsql:latest alex/apsql
-docker run --name apsql -p 5432:5432 -h apsql apsql /usr/bin/supervisord -n
+docker run -d --name apsql -p 5432:5432 -h apsql apsql /usr/bin/supervisord -n
 ansible-playbook alex/apsql/p.yml
 docker build -t arsum:latest alex/arsum
-docker run --name arsum -p 8192:8192 -h arsum --link apsql arsum /usr/bin/supervisord -n
+docker run -d --name arsum -p 8192:8192 -h arsum --link apsql arsum /usr/bin/supervisord -n
 ansible-playbook alex/arsum/p.yml
 docker build -t angos:latest alex/angos
-docker run --name angos -p 8192:8192 -h angos --link apsql --link arsum angos /usr/bin/supervisord -n
+docker run -d --name angos -p 8192:8192 -h angos --link apsql --link arsum angos /usr/bin/supervisord -n
 ansible-playbook alex/angos/p.yml
