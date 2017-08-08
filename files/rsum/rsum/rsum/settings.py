45782f36eed1f59d2f2d8a5894770e4f0a908e84
Edward Harris <cfo@ecla.solutions>
2017-08-08 02:19:23 +0000

    Update settings. See #64.

--- files/rsum/rsum/rsum/settings.py
+++ files/rsum/rsum/rsum/settings.py
@@ -87,14 +87,16 @@ WSGI_APPLICATION = 'rsum.wsgi.application'
 if socket.gethostname() == 'mrsum': 
     DB_HOST = 'mpsql'
     DB_PORT = '5433'
+    DB_NAME = 'mrsum'
 elif socket.gethostname() == 'arsum':
     DB_HOST = 'apsql'
     DB_PORT = '5432'
+    DB_NAME = 'arsum'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'alex',
+        'NAME': DB_NAME,
         'USER': 'psql',
         'PASSWORD': '',
         'HOST': DB_HOST, 
