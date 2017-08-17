bc3aae4fa179d84925f169bfdac8fe05f8b3c82c
A. H. Laughlin <alex@ecla.solutions>
2017-08-17 13:38:49 -0700

    Fix syntax error. See #92.

--- files/deploy/deploy.bash
+++ files/deploy/deploy.bash
@@ -6,9 +6,7 @@ export DD=`date +%Y%m%d-%H%M%s`
 docker commit -m "Archive arsum for deploy." arsum ecla/arsum:$DD
 docker push ecla/arsum:$DD
 docker rmi ecla/arsum:$DD
-docker rm -f arsum
-docker rmi -f arsum:latest
-docker build -t arsum:latest /src/rsum/files/alex/arsum
+docker build -t arsum:latest /src/rsum/files/deploy/alex/
 /opt/py/bin/ansible-galaxy remove arsum 
 /opt/py/bin/ansible-galaxy install --force -r /src/rsum/files/deploy/alex/requirements.yml
 docker run -d --network arsum --name arsum -p 8192:8192 -h arsum arsum /usr/bin/supervisord -n
