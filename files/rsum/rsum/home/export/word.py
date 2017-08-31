beb5fc0102f1a4d4f016e3766114aec9b5f10af1
Edward A. Harris <alex@ecla.solutions>
2017-08-31 02:53:42 +0000

    Progress on adding the summary. See #100.

--- files/rsum/rsum/home/export/word.py
+++ files/rsum/rsum/home/export/word.py
@@ -11,6 +11,7 @@ from docx.shared import Pt
 from docx.shared import RGBColor
 from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.table import WD_TABLE_ALIGNMENT
 
 import home.schema
 
@@ -29,44 +30,48 @@ class ExportDocument(object):
         document = self.set_styles(document)
         document = self.set_layout(document)
 
-        for current,section in enumerate(cv.get('sections')):
-            #document.add_heading(section.get('name'))
-            #self.add_section(section.get('content'), document)
-
+        for current,section in sorted(enumerate(cv.get('sections'))):
             if section.get('name') == u'intro':
-                self.add_intro(section.get('content'), document)
+                document = self.add_intro(section.get('content'), document)
+
+            if section.get('name') == u'summary':
+                document.add_paragraph('')
+                document = self.add_summary(section.get('content'), document)
 
         document.save(stream)
 
         return stream 
 
-    def add_section(self, section, document):
-        for subsection in section:
-            document.add_heading(subsection.get('name').replace('_',' '), level=2)
-            if (
-                isinstance(subsection.get('content'), list) and
-                len(subsection.get('content')) > 0
-            ):
-                content = subsection.get('content')[0].get('content')
-                print(type(content))
-                document.add_paragraph(content)
-
     def add_intro(self, intro, document):
         table = document.add_table(rows=1, cols=2)
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+        print(table.alignment)
         for index, item in enumerate(sorted(intro)):
-            print(item.get('content'))
             if len(item.get('content')) == 1:
                 content = item.get('content')[0].get('content')
                 table.cell(0,0).add_paragraph(content, style='Heading '+str(index+1))
             else:
-                print(item.get('content'))
+                # Need to add image linking support to docx.
+                # print(item.get('content'))
+                pass
 
-                
-        table.cell(0,0).width = Cm(14)
+        table.cell(0,0).width = Cm(12)
         table.cell(0,1).add_picture('/srv/rsum/static/acv/img/mockup/avatar-01.png', width=Cm(4))
         table.cell(0,1).width = Cm(4)
         table.cell(0,1).paragraphs[0].paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-        return None
+        return document 
+
+    def add_summary(self, summary, document):
+        t = document.add_table(rows=1, cols=2)
+        t.alignment = WD_TABLE_ALIGNMENT.CENTER
+        for index, item in enumerate(sorted(summary)):
+            if len(item.get('content')) == 1:
+                t.cell(0,0).add_picture('/srv/rsum/static/acv/img/500x700/01.jpg', width=Cm(5))
+                t.cell(0,1).add_paragraph('Summary', style='Heading 1')
+                t.cell(0,1).add_paragraph(item.get('content')[0].get('content'), style='Normal')
+                t.cell(0,1).width = Cm(11)
+                print(item)
+        return document 
 
     def set_styles(self, document):
         document.styles['Heading 1'].delete()
@@ -86,6 +91,13 @@ class ExportDocument(object):
         font.color.rgb = RGBColor(0xA6, 0xA7, 0xAA)
         font.size = Pt(16)
 
+        document.styles['Normal'].delete()
+        document.styles.add_style('Normal', WD_STYLE_TYPE.PARAGRAPH, builtin=True)
+        style = document.styles['Normal']
+        font = style.font
+        font.name = 'Hind'
+        font.color.rgb = RGBColor(0xA6, 0xA7, 0xAA)
+        font.size = Pt(12)
         return document
 
     def set_layout(self, document):
