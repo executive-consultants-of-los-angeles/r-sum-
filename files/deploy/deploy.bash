dad6798c515a209452c773287a5cad6ea12560cf
Alexander H. Laughlin <mal@gahan-corporation.com>
2017-09-05 18:06:28 -0700

    Add tmrsum. See #109.

--- files/deploy/deploy.bash
+++ files/deploy/deploy.bash
@@ -20,5 +20,12 @@ docker build -t jrsum:latest /src/rsum/files/deploy/jess
 # docker run -d --network arsum --name jrsum -p 8704:8704 -h jrsum jrsum /usr/bin/supervisord -n
 /opt/py/bin/ansible-playbook /src/rsum/files/deploy/jess/playbook.yml
 
+# tmrsum
+docker build -t tmrsum:latest /src/rsum/files/deploy/alex-tm/
+/opt/py/bin/ansible-galaxy remove tmrsum 
+/opt/py/bin/ansible-galaxy install --force -r /src/rsum/files/deploy/alex/requirements.yml
+# docker run -d --network arsum --name arsum -p 8192:8192 -h arsum arsum /usr/bin/supervisord -n
+/opt/py/bin/ansible-playbook /src/rsum/files/deploy/alex/playbook.yml
+
 # angnx
 /opt/py/bin/ansible -m shell -a 'ansible-galaxy install -r /tmp/arsum.yml --force' angnx
