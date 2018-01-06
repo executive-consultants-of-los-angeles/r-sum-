a5aa7978f2a0cda2165e7c8cd263fc871501283b
Xander Harris <duchess@gahan-corporation.com>
2018-01-06 06:08:23 +0000

    Adjust imports. See #157.

--- rsum/home/export/word.py
+++ rsum/home/export/word.py
@@ -18,9 +18,7 @@ from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx.enum.style import WD_BUILTIN_STYLE
 
-import home.models
-
-Profile = home.models.profile.Profile
+from home.models.profile import Profile
 
 
 class ExportDocument(object):
