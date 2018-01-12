b8337c23697dcf1f5f420c5674b4f6c71d5f810b
Xander Harris <duchess@gahan-corporation.com>
2018-01-12 04:50:15 +0000

    Closer to actual tests. See #161.

--- rsum/home/tests/test_export.py
+++ rsum/home/tests/test_export.py
@@ -1,7 +1,16 @@
 """Test module for export class."""
+import pytest
+from home.export.word import ExportDocument
 
 
-def test_export():
-    """Run the export method."""
-    if not True:
+def test_export(export_document):
+    """Test the object."""
+    print(export_document)
+    if not isinstance(export_document, ExportDocument):
         raise AssertionError()
+
+
+@pytest.mark.usefixtures('db')
+def test_run_export(export_document):
+    """Run the export method and see what happens."""
+    print(export_document.export(1))
