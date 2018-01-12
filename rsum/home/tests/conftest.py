cfd55cd422793f656b1b765a4e378c04332fd975
Xander Harris <duchess@gahan-corporation.com>
2018-01-12 04:39:09 +0000

    Pytest-django fixes it. See #161.

--- rsum/home/tests/conftest.py
+++ rsum/home/tests/conftest.py
@@ -1,11 +1,14 @@
 """pytest configuration module."""
-import django
 import pytest
 
+import django
+from django.conf import settings
+
+from home import models
 from home.export.word import ExportDocument
 
-
 django.setup()
+
 
 
 @pytest.fixture(scope="session")
