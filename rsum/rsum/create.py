dbd9a2239d0b2b6603515b3d54d5633212e37a84
Xander Harris <duchess@gahan-corporation.com>
2018-01-12 13:36:02 +0000

    Adjust to use tempfile. See #186.

--- rsum/rsum/create.py
+++ rsum/rsum/create.py
@@ -1,12 +1,12 @@
 """Module creates database entries for a profile."""
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rsum.settings")
+import tempfile
 import django
-django.setup()
+
 from home.models.profile import Profile
+
+django.setup()
 
 Profile.create()
 
-with open('/tmp/ready', 'w') as ready:
-    ready.write('ready')
-    ready.close()
+with tempfile.TemporaryFile() as tmpf:
+    tmpf.write('ready')
