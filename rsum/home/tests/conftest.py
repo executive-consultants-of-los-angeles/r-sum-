2072ac1b6ac0d9f9e2a5fac12d419936517ac5eb
Xander Harris <duchess@gahan-corporation.com>
2018-01-12 04:08:25 +0000

    Add fixture for export. See #161.

--- rsum/home/tests/conftest.py
+++ rsum/home/tests/conftest.py
@@ -1,14 +1,10 @@
 """pytest configuration module."""
 import pytest
 
-
-class DB(object):
-    """A DB object."""
-
-    pass
+from home.export.word import ExportDocument
 
 
 @pytest.fixture(scope="session")
-def db():
+def export_document():
     """Return a DB object."""
-    return DB()
+    return ExportDocument()
