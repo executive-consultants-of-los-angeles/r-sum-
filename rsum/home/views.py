4376bc82504065d3d0a36297cb28f2acc2957493
Xander Harris <duchess@gahan-corporation.com>
2018-01-19 06:47:37 +0000

    Interpolate the string anyway. See #197.

--- rsum/home/views.py
+++ rsum/home/views.py
@@ -3,7 +3,6 @@
 """Views for the rsum home application."""
 import datetime
 import json
-import os
 from collections import OrderedDict
 
 from django.shortcuts import render
@@ -68,7 +67,7 @@ def export_docx(cv_id='1'):
         )
     )
     response['Content-Disposition'] = (
-        'attachment; filename={0}-cv.docx'.format(xander)
+        'attachment; filename={0}-cv.docx'.format('xander')
     )
     response['Content-Length'] = length
     return response
