9d0dd255c731e88c90e451486c8ebca263e735fd
Xander Harris <duchess@gahan-corporation.com>
2018-01-19 05:42:45 +0000

    Try to configure db. See #197.

--- rsum/rsum/loadapps.py
+++ rsum/rsum/loadapps.py
@@ -1,11 +1,25 @@
 """Module creates database entries for a profile."""
 import django
+import dj_database_url
 from django.conf import settings
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xander',
+        'USER': 'xander',
+        'PASSWORD': '',
+        'HOST': 'pg.gc',
+        'PORT': '5432',
+    }
+}
 
 
 def main():
     try:
-        settings.configure()
+        db_from_env = dj_database_url.config(conn_max_age=500)
+        DATABASES['default'].update(db_from_env)
+        settings.configure(DATABASES=DATABASES)
     except RuntimeError:
         pass
     return django.setup()
