b69e3e78dc086298bf74deba2d5dfb5aeb027d5c
Xander Harris <duchess@gahan-corporation.com>
2018-01-20 01:36:45 -0800

    Add missing static setting. See #212.

--- rsum/rsum/settings.py
+++ rsum/rsum/settings.py
@@ -113,6 +113,7 @@ STATIC_URL = '/static/'
 DEFAULT_FILE_STORAGE = "django_s3_storage.storage.S3Storage"
 STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
 AWS_S3_BUCKET_NAME = "rsum.gahan-corporation.com"
+AWS_S3_BUCKET_NAME_STATIC = "rsum.gahan-corporation.com"
 AWS_REGION = "us-west-2"
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
