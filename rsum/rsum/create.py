6afc03b2fda38b7c4d6004a6603e3ed5524916f8
Xander Harris <duchess@gahan-corporation.com>
2018-01-20 08:47:38 -0800

    Delete before create. See #173.

--- rsum/rsum/create.py
+++ rsum/rsum/create.py
@@ -1,4 +1,5 @@
 """Module creates database entries for a profile."""
+import os
 import tempfile
 import loadapps
 
@@ -6,6 +7,7 @@ from home.models.profile import Profile
 
 loadapps.main()
 
+Profile.objects.filter(name=os.environ.get('RSUM_ENV')).delete()
 Profile.create()
 
 with tempfile.TemporaryFile() as tmpf:
