fa948f069e38d88205aef387d61b9c315fce27f4
Xander Harris <duchess@gahan-corporation.com>
2018-01-20 00:29:43 -0800

    Add config file. See #212.

---
+++ rsum/rsum/aws.py
@@ -0,0 +1,13 @@
+"""AWS Settings"""
+
+# The AWS region to connect to.
+AWS_REGION = "us-east-1"
+
+# The AWS access key to use.
+AWS_ACCESS_KEY_ID = ""
+
+# The AWS secret access key to use.
+AWS_SECRET_ACCESS_KEY = ""
+
+# The optional AWS session token to use.
+AWS_SESSION_TOKEN = ""
