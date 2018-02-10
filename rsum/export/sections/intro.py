a90540b38a397c288cc6f4b06ae310102c66bf75
Xander Harris <duchess@gahan-corporation.com>
2018-02-09 22:00:17 -0800

    Drop photo from intro. See #248.

--- rsum/export/sections/intro.py
+++ rsum/export/sections/intro.py
@@ -1,8 +1,6 @@
 """Intro module."""
 # pylint: disable=too-few-public-methods,no-name-in-module,no-member
 from django.conf import settings as django_settings
-from docx.shared import Cm
-from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 
@@ -25,27 +23,12 @@ class Intro(object):
         """
         intro = section
         self.name = name
-        settings = self.settings
-        table = document.add_table(rows=1, cols=2)
-        table.cell(0, 0).width = Cm(12)
+        paragraph = document.add_paragraph(
+            intro.get('name'), style='Heading 1')
 
-        table.alignment = WD_TABLE_ALIGNMENT.CENTER
-        table.cell(0, 0).add_paragraph(
-            intro.get('name'),
-            style='Heading 1'
-        )
-        table.cell(0, 0).add_paragraph(
-            intro.get('position'),
-            style='Heading 2')
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.LEFT
 
-        table.cell(0, 1).width = Cm(4)
-        table.cell(0, 1).add_picture(
-            'static/profiles/{0}/img/mockup/avatar-02.png'.format(
-                settings.DIR))
-        table.cell(
-            0,
-            1
-        ).paragraphs[
-            0
-        ].paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        paragraph = document.add_paragraph(
+            intro.get('position'), style='Heading 2')
+
         return document
