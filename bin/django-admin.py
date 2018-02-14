c200ea19bace4366f836e3591995a5fbba4f11fc
Xander Harris <duchess@gahan-corporation.com>
2018-02-13 19:42:49 -0800

    Drop virtualenv. See #239.

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Volumes/killface/gahan-corporation/department-of-information/source/rsum/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
